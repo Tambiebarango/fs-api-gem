0904ff094ce17d62553e2a0ae867bdaac93159bf
Tambiebarango <theodore.ambiebarango@fullscript.com>
2021-12-21 11:28:08 -0500

    Add test method

--- lib/fullscriptapi.rb
+++ lib/fullscriptapi.rb
@@ -1,7 +1,30 @@
 require_relative "fullscriptapi/version"
+require 'excon'
 
-class FullscriptApi
-  def initialize
-    pp "We are connected now"
+module FullscriptApi
+  class << self
+    attr_accessor :client_id, :secret, :redirect_uri, :token, :server
+    
+    def config
+      yield self if block_given?
+
+      self
+    end
+    alias :new :config
+
+    def use_token(token)
+      @token = token
+    end
+
+    def list_all_products
+      response = Excon.get("#{server}/api/catalog/products",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer #{@token}"
+        }
+      )
+
+      response
+    end
   end
 end
